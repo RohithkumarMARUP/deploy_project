0991de5658a5a6639739fc1e70e9ebca90874880
RohithkumarMARUP <rohithkumarsep92@gmail.com>
2023-02-10 12:05:05 +0530

    2:------>4th

--- packages/regression_model/regression_model/config/config.py
+++ packages/regression_model/regression_model/config/config.py
@@ -4,7 +4,7 @@ import regression_model
 
 
 PACKAGE_ROOT = pathlib.Path(regression_model.__file__).resolve().parent
-TRAINED_MODEL_DIR = PACKAGE_ROOT /'regression_model'/ "trained_models"
+TRAINED_MODEL_DIR = PACKAGE_ROOT /"trained_models"
 DATASET_DIR = PACKAGE_ROOT / "datasets"
 
 # data

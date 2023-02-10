77ba979b264f955fb3bfb39fb3059459ca8eb7c9
RohithkumarMARUP <rohithkumarsep92@gmail.com>
2023-02-10 13:44:20 +0530

    2:------>14th

--- packages/regression_model/regression_model/predict.py
+++ packages/regression_model/regression_model/predict.py
@@ -10,7 +10,7 @@ import logging
 
 _logger = logging.getLogger(__name__)
 
-pipeline_file_name = f"{config.PIPELINE_SAVE_FILE}{_version}"
+pipeline_file_name = f"{config.PIPELINE_SAVE_FILE}{_version}.pkl"
 _price_pipe = load_pipeline(file_name=pipeline_file_name)
 
 
